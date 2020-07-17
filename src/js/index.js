9805a5bfce28330f630d6e2cb96d1baa6f20e0ff
kawa2019 <kamil.ku@vp.pl>
2020-07-17 16:14:30 +0200

    adding product to another category

--- src/js/index.js
+++ src/js/index.js
@@ -2,29 +2,38 @@ import * as mdb from 'mdb-ui-kit';
 // import { doc } from 'prettier';
 document.addEventListener('DOMContentLoaded', () => {
   const select = document.querySelector('select');
-  const categoryArray = [
-    'warzywa',
-    'owoce',
-    'nabiał',
-    'pieczywo',
-    'artykuły higieniczne',
-    'napoje',
-  ];
+  const containerShoppingList = document.querySelector('.container-shoppingList');
+  const quantityProductLabel = document.querySelector('label[for="quantityProductLabel"]');
+  const counter = document.querySelector('select').options;
+  const categoryArray = ['warzywa', 'owoce', 'nabiał', 'pieczywo', 'artykuł-higieniczne', 'napoje'];
   categoryArray.map((category) => {
     const newCategory = document.createElement('option');
     newCategory.innerHTML = category;
-    return select.appendChild(newCategory);
+    const newCategoryList = document.createElement('ul');
+    newCategoryList.classList.add(category);
+    const allReturn = [
+      select.appendChild(newCategory),
+      containerShoppingList.appendChild(newCategoryList),
+    ];
+    return allReturn.map((reTurn) => reTurn);
   });
+  const kindOfCounter = () => {
+    if (counter[0].selected || counter[1].selected) {
+      return (quantityProductLabel.innerHTML = 'ciężar w kg:');
+    }
 
+    return (quantityProductLabel.innerHTML = 'sztuk:');
+  };
+  select.addEventListener('change', kindOfCounter);
   const addProduct = (event) => {
     event.preventDefault();
-    const numbers = /^[0-9]+$/;
-    //
+    const product = document.querySelector('#product');
     const quantityProduct = document.querySelector('#quantityProduct');
-    if (numbers.test(quantityProduct.value)) {
-      console.log('quantityProduct');
-      return;
-    }
+    const chooseCategory = [...counter].find((option) => option.selected === true).innerHTML;
+    const shoppingList = document.querySelector(`.${chooseCategory}`);
+    const newProduct = document.createElement('li');
+    newProduct.innerHTML = `${product.value} ${quantityProductLabel.innerHTML}${quantityProduct.value}`;
+    return shoppingList.appendChild(newProduct);
   };
   document.querySelector('form').addEventListener('submit', addProduct);
 });
